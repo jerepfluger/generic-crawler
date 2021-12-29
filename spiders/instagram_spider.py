91f6ec738a032ae7750232ce1443986b72c22b51
Jeremias Pfluger <jeremiaspfluger@Jeremias-Pfluger.local>
2021-12-29 19:52:32 -0300

    adding request example file

--- spiders/instagram_spider.py
+++ spiders/instagram_spider.py
@@ -14,6 +14,8 @@ from selenium.webdriver.support.select import By
 from spiders.spider import Spider
 
 
+INSTAGRAM_URL = "https://www.instagram.com/"
+
 emojis = ['😀', '😃', '😄', '😁', '😆', '😅', '😂', '🤣', '🥲', '☺️', '😊', '😇', '🙂', '🙃', '😉', '😌', '😍', '🥰',
           '😘', '😗', '😙', '😚', '😋', '😛']
 
@@ -25,7 +27,7 @@ class InstagramSpider(Spider):
     def process_task(self, crawling, web_driver_pool):
         driver = web_driver_pool.acquire(None, self._config.get('webdriver'))
         # Go to instagram website
-        driver.get(self._config.get('base_url'))
+        driver.get(INSTAGRAM_URL)
 
         # Waiting for login page to be fully loaded
         WebDriverWait(driver, 5).until(
@@ -35,7 +37,7 @@ class InstagramSpider(Spider):
         # Waiting for user to be fully logged in and redirecting to
         WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located((By.XPATH, self._config.get('html_location.user_avatar'))))
-        driver.get("{}{}".format(self._config.get('base_url'), crawling['data']['desired_post']))
+        driver.get("{}{}".format(INSTAGRAM_URL, crawling['data']['desired_post']))
 
         if crawling['data']['needs_tagging']:
             self._tag_friends(driver, crawling)
@@ -62,11 +64,11 @@ class InstagramSpider(Spider):
 
     def _tag_friends(self, driver, crawling):
         subset_count = 0
-        combinations_array = itertools.combinations(crawling['data']['friends'], crawling['data']['tags_needed'])
-        combinations_array_length = len(list(combinations_array))
+        combinations_array = list(itertools.combinations(crawling['data']['friends'], crawling['data']['tags_needed']))
 
         for subset in combinations_array:
-            comment_button = driver.find_element_by_xpath(self._config.get('html_location.comment_button'))
+            comment_button = driver.find_element_by_xpath(".//span[@class='_15y0l']/button")
+            # comment_button = driver.find_element_by_xpath(self._config.get('html_location.comment_button'))
             comment_button.click()
 
             comment_area = driver.switch_to.active_element
@@ -85,23 +87,25 @@ class InstagramSpider(Spider):
                 driver.find_element_by_xpath(self._config.get('html_location.blocked_banner'))
                 logger.error('Last element able to be posted was -> {}'.format(subset))
                 logger.error('From a total of {}/{} and this represent the {} percentage'.format(subset_count,
-                                                                                                 combinations_array_length,
-                                                                                                 subset_count * 100 / combinations_array_length))
+                                                                                                 len(combinations_array),
+                                                                                                 subset_count * 100 / len(combinations_array)))
                 # Aca posiblemente podes hacer un sleep mas largo en caso de fallo y poner un timeout a los X reintentos
                 # Otra opcion sería hacer un skip de la cuenta que estamos por taggear, pero esa logica seria un poco mas rebuscada
                 return Response('Procedure ended up with ERRORS!', 429)
             except NoSuchElementException:
                 subset_count += 1
                 logger.info('Successfully commented %s/%s with tags %s and message %s', subset_count,
-                            combinations_array_length, subset, crawling['data']['message'])
+                            len(combinations_array), subset, crawling['data']['message'])
                 pass
 
             # Waiting for comment to be published
             WebDriverWait(driver, 5).until(
                 EC.element_to_be_clickable((By.XPATH, self._config.get('html_location.text_area'))))
 
-            if subset_count < combinations_array_length:
+            if subset_count < 419:
                 time.sleep(40)
+            else:
+                logger.info('Already running for 5 hours. Seems comments enough by this time')
 
     def _follow_account(self, driver):
         if not self._already_following(driver):
