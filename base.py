a3c8542e56a3d36423e49d17466c5052310767dd
Jeremias Pfluger <jeremiaspfluger@Jeremias-Pfluger.local>
2022-01-18 17:50:44 -0300

    pr fixes

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# TODO: This needs to be set by config
+# FIXME: GC-13 // This needs to be set by config
 engine = create_engine('mysql://root:J3r3mia$123@localhost:3306/generic_crawler')
 Session = sessionmaker(bind=engine)
 
